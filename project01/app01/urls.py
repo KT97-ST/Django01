492ba9ce31e8a0d60b41c23606682c65b4a8691b
kthanhktst <74465408+kthanhktst@users.noreply.github.com>
2023-03-22 21:54:44 +0700

    #1

--- project01/app01/urls.py
+++ project01/app01/urls.py
@@ -1,8 +1,11 @@
-from django.contrib import admin
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('listquestion/', views.viewlistquestion, name="viewlistquestion"),
+    path('detailquestion/<int:question_id>', views.detailView, name="detail"),
+    path('choicevote/<int:question_id>', views.choiceVote, name="choicevote"),
+
 ]
--- project01/app01/views.py
+++ project01/app01/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_list_or_404
-
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 from .models import Question
 
 
@@ -13,6 +13,29 @@ def index(request):
 
 
 def viewlistquestion(request):
-    list_question = get_list_or_404(Question, pk=1)
-    context = {"list_question": list_question}
+    # list_question = get_list_or_404(Question, pk=1)
+    list_question = Question.objects.all()
+    context = {"list_question": list_question,
+               "title": "Tra Loi Cau Hoi"}
     return render(request, "app01/question_list.html", context)
+
+
+def detailView(request, question_id):
+    q = Question.objects.get(pk=question_id)
+    context = {
+        "qs": q,
+        "name": "Chu tich",
+        "questiontext": q.question_text,
+        "title": "Tra Loi Cau Hoi"}
+    return render(request, "app01/detail_question.html", context)
+
+def choiceVote(request, question_id):
+    q = Question.objects.get(pk=question_id)
+    try:
+        rq = request.POST["choice"]
+        c = q.choice_set.get(pk=rq)
+        c.vote = c.vote + 1
+        c.save()
+    except:
+        HttpResponse("Khong tim thay doi tuong")
+    return render(request, "app01/result.html", {"q":q})
